984612b1ff6c3648081dc32b62dfbfc5c30075f0
JohnnyLFisher <johnny.luel.fisher@gmail.com>
2020-11-12 13:58:59 -0600

    unit tests for isEven

--- code.js
+++ code.js
@@ -5,12 +5,16 @@ const helloWorld = function(){
 //sayHello function
 function sayHello(name){
 	if ((typeof name !== "string") || name === ""){
-		return "Hello, World!"
+		return "Hello, World!";
 	} else {
 		return "Hello, " + name + "!";
 	}
 };
 //isFive function
 function isFive(num) {
-	return num == 5
+	return num == 5;
+};
+//isEven function
+function isEven(num){
+	return parseFloat(num) % 2 === 0
 };
--- tests.js
+++ tests.js
@@ -61,3 +61,39 @@ describe("sayHello", function () {
 		 expect(isFive("")).toBe(false);
 	 });
  });
+
+ describe("isEven", function () {
+	 it('should be a defined function', function () {
+		 expect(typeof isEven).toBe("function");
+	 });
+	 it('should return a boolean no matter what', function () {
+		 expect(typeof isEven()).toBe("boolean");
+	 });
+	 it('should return true when given 2', function () {
+		 expect(isEven(2)).toBe(true);
+	 });
+	 it('should return true when given -4', function () {
+		 expect(isEven(-4)).toBe(true);
+	 });
+	 it('should return false when given 3', function () {
+		 expect(isEven(3)).toBe(false);
+	 });
+	 it('should return false when given "banana"', function () {
+		 expect(isEven("banana")).toBe(false);
+	 });
+	 it('should return true when given "8"', function () {
+		 expect(isEven(8)).toBe(true);
+	 });
+	 it('should return false when given Infinity', function () {
+		 expect(isEven(Infinity)).toBe(false);
+	 });
+	 it('should return false when given false', function () {
+		 expect(isEven(false)).toBe(false);
+	 });
+	 it('should return false when given true', function () {
+		 expect(isEven(true)).toBe(false);
+	 });
+	 it('should return false when called without an argument', function () {
+		 expect(isEven()).toBe(false);
+	 });
+ });
