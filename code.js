116ef0154c7ae7582f1d1fa048893cdb95ccb066
JohnnyLFisher <johnny.luel.fisher@gmail.com>
2020-11-12 14:52:07 -0600

    unit tests for add

--- code.js
+++ code.js
@@ -18,3 +18,33 @@ function isFive(num) {
 function isEven(num){
 	return parseFloat(num) % 2 === 0
 };
+//isVowel function
+function isVowel(letter) {
+	if (letter === "a"){
+		return true
+	} else if (letter === "A"){
+		return true
+	} else if (letter === "e"){
+		return true
+	} else if (letter === "E"){
+		return true
+	} else if (letter === "i"){
+		return true
+	} else if (letter === "I"){
+		return true
+	} else if (letter === "o"){
+		return true
+	} else if (letter === "O"){
+		return true
+	} else if (letter === "u"){
+		return true
+	} else if (letter === "U"){
+		return true
+	} else {
+		return false
+	}
+};
+//add function
+function add(num1,num2) {
+		return parseFloat(num1) + parseFloat(num2);
+};
