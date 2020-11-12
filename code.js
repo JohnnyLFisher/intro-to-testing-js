5e8393ab96f1facac7868eb55d3dcbef8165024d
JohnnyLFisher <johnny.luel.fisher@gmail.com>
2020-11-12 12:01:15 -0600

    unit tests for edge cases.

--- code.js
+++ code.js
@@ -3,12 +3,9 @@ const helloWorld = function(){
 	return "Hello, World!";
 };
 function sayHello(name){
-	// if(name === "Alex"){
-	// 	return "Hello, Alex!"
-	// } else if (name === "Pat") {
-	// 	return "Hello, Pat!"
-	// } else {
-	// 	return "Hello, Jane!";
-	// }
-	return "Hello, " + name + "!";
+	if ((typeof name !== "string") || name === ""){
+		return "Hello, World!"
+	} else {
+		return "Hello, " + name + "!";
+	}
 };
